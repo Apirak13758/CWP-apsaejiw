230d2c7bd901f0daddfcfcc701d13257fb1098aa
Apirak13758 <66070218@kmitl.ac.th>
2026-09-22 15:46:23 +0700

    fix matching param

--- discovery_piscine/cell05/ex10/parameter_matching.py
+++ discovery_piscine/cell05/ex10/parameter_matching.py
@@ -2,7 +2,7 @@ import sys
 import re
 count = 0
 if len(sys.argv) > 1: 
-    if len(re.findall(sys.argv[1], input(), re.IGNORECASE)) > 0:
+    if len(re.findall(sys.argv[1], input("What was the parameter? "), re.IGNORECASE)) > 0:
         print("Good job!")
     else:
         print("Nope, sorry...")
