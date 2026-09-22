08c12dc62bb86394c574619b797231742051a030
Apirak13758 <66070218@kmitl.ac.th>
2026-09-22 14:30:55 +0700

    fix 5-11 bug

--- discovery_piscine/cell05/ex11/count_it.py
+++ discovery_piscine/cell05/ex11/count_it.py
@@ -1,6 +1,7 @@
 import sys
 if len(sys.argv) > 1: 
-    for i in range(0, len(sys.argv)):
+    print(f"parameters: {len(sys.argv)-1}")
+    for i in range(1, len(sys.argv)):
         print(f"{sys.argv[i]}: {len(sys.argv[i])}")
 else:
     print("none")
