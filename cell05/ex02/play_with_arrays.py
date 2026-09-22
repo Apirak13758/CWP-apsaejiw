b4785f03bb45f5d3cac8a51509d9ed9d14677ed8
Apirak13758 <66070218@kmitl.ac.th>
2026-09-23 01:14:29 +0700

    cell50.2-3 array[i] not + 2 in if condition

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -1,4 +1,4 @@
 array = [2, 8, 9, 48, 8, 22, -12, 2]
-new_array = [array[i] + 2 for i in range(len(array)) if array[i] + 2 > 5]
+new_array = [array[i] + 2 for i in range(len(array)) if array[i] > 5]
 print(array)
 print(new_array)
--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,5 +1,5 @@
 array = [2, 8, 9, 48, 8, 22, -12, 2]
 print(array)
 
-new_array = {array[i] + 2 for i in range(len(array)) if array[i] + 2 > 5}
+new_array = {array[i] + 2 for i in range(len(array)) if array[i] > 5}
 print(new_array)
