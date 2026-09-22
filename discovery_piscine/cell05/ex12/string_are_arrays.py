c6cbbba6c3844e0bec515c24655ce35f8c08e271
Apirak13758 <66070218@kmitl.ac.th>
2026-09-22 15:44:42 +0700

    fix z find

--- discovery_piscine/cell05/ex12/string_are_arrays.py
+++ discovery_piscine/cell05/ex12/string_are_arrays.py
@@ -1,7 +1,11 @@
 import sys
+count = 0
 if len(sys.argv) > 1: 
     for i in sys.argv[1]:
         if i == 'z':
             print("z", end="")
+            count += 1
+    if count == 0:
+        print("none")
 else:
     print("none")
