409ea130923757b394918c46d0086382d77509dc
Apirak13758 <66070218@kmitl.ac.th>
2026-09-22 19:40:09 +0700

    refactor array

--- discovery_piscine/cell05/ex01/play_with_arrays.py
+++ discovery_piscine/cell05/ex01/play_with_arrays.py
@@ -1,6 +1,4 @@
 array = [2, 8, 9, 48, 8, 22, -12, 2]
-new_array = []
-for i in range(len(array)):
-    new_array.append(array[i] + 2)
+new_array = [array[i] + 2 for i in range (len(array))]
 print(f"Original array: {array}")
 print(f"New array: {new_array}")
--- discovery_piscine/cell05/ex03/play_with_arrays.py
+++ discovery_piscine/cell05/ex03/play_with_arrays.py
@@ -1,12 +1,5 @@
 array = [2, 8, 9, 48, 8, 22, -12, 2]
-new_array = []
-dict = {}
-final_array = []
-for i in range(len(array)):
-    if array[i] + 2 > 5:
-        new_array.append(array[i] + 2)
-        dict[array[i] + 2] = 0
-for key in dict:
-    final_array.append(key)
 print(array)
-print(final_array)
+
+new_array = {array[i] + 2 for i in range(len(array)) if array[i] + 2 > 5}
+print(new_array)
